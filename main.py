a2487fcf0e49c6f72dc4735d1b897a98e9d109f2
kot228begemot <78966260+kot228begemot@users.noreply.github.com>
2023-04-23 16:42:13 +0300

    перед правками

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import openpyxl
+import progressbar
 class ExelExpenses:
     def __init__(self):
         self. _book = openpyxl.open("C:/Users/apec9/Dropbox/ExelExpenses/test.xlsx", read_only=True)
         self.list_number = self._book.worksheets[0]
-        self._storage_of_category = {'еда': {},
-                                     'Инвестиции': {},
-                                     'Алкоголь': {},
-                                     'Транспорт': {},
-                                     'Продукты': {},
-                                     'Рестораны': {},
-                                     'Одежда': {},
-                                     'Здоровье, красота, гигиена': {},
-                                     'Кино, театры, музеи, развлечения': {},
-                                     'Связь': {},
-                                     'Квартира': {},
-                                     'Творчество, книги, обучение, спорт': {},
-                                     'Прочее': {}}
+        self._storage_of_category = {}
     def expenses_processing(self, month, year):
         error_positions = []
-        start_month_line = self.find_border_of_the_month(month, year)
+        start_end_month_line = self.find_border_of_the_month(month, year)
+        self.category_counter(start_end_month_line)
         a=1
 
     def find_border_of_the_month(self, month, year):
@@ -42,201 +32,87 @@ class ExelExpenses:
                     flag_find_year = True
                     break
                 middle = (_position_of_start_line + _position_of_end_line) // 2
+                print(middle)
                 if self.list_number[middle][_position_of_general_data_column].value.year < year:
                     _position_of_start_line = middle
                 else:
                     _position_of_end_line = middle
-            flag_find_month = False
-            _position_of_start_line = start_year_line
-            _position_of_end_line = self.list_number.max_row
-            if month == 1:
-                start_month_line = start_year_line
-                flag_find_month = True
-            else:
-                while flag_find_month == False:
-                    if (self.list_number[_position_of_start_line][
-                            _position_of_general_data_column].value.month == month - 1 and \
-                        self.list_number[_position_of_start_line + 1][
-                            _position_of_general_data_column].value.month == month) or (
-                            middle == start_year_line and self.list_number[_position_of_start_line - 1][
+        flag_find_start_month = False
+        _position_of_start_line = start_year_line
+        _position_of_end_line = self.list_number.max_row
+        if month == 1:
+            start_month_line = start_year_line
+            flag_find_start_month = True
+        else:
+            while flag_find_start_month == False:
+                if (self.list_number[_position_of_start_line][
+                        _position_of_general_data_column].value.month == month - 1 and \
+                    self.list_number[_position_of_start_line + 1][
+                        _position_of_general_data_column].value.month == month) or (
+                        middle == start_year_line and self.list_number[_position_of_start_line - 1][
+                    _position_of_general_data_column].value.month != month):
+                    start_month_line = _position_of_start_line
+                    flag_find_start_month = True
+                    break
+                middle = (_position_of_start_line + _position_of_end_line) // 2
+                print(middle)
+                if self.list_number[middle][_position_of_general_data_column].value.month < month:
+                    _position_of_start_line = middle
+                else:
+                    _position_of_end_line = middle
+
+        flag_find_end_month = False
+        _position_of_end_line = self.list_number.max_row
+        _position_of_start_line = start_month_line
+
+        while flag_find_end_month == False:
+            if (self.list_number[middle][
+                _position_of_general_data_column].value.month == month and \
+                    self.list_number[middle + 1][
                         _position_of_general_data_column].value.month != month):
-                        start_year_line = _position_of_start_line
-                        flag_find_year = True
-                        break
-                    middle = (_position_of_start_line + _position_of_end_line) // 2
-                    if self.list_number[middle][_position_of_general_data_column].value.month < month:
-                        _position_of_start_line = middle
-                    else:
-                        _position_of_end_line = middle
-            return start_year_line
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def category_counter(month: int, year: int, write_exel: bool = False):
-    """
-    :param month: месяц, для которого надо произвести расчёты
-    :param year: год, для которого надо произвести расчёты
-    :return: словарь категорий, подкатегорий и сумм
-    """
-    book = openpyxl.open("C:/Users/apec9/Dropbox/ExelExpenses/test.xlsx", read_only=True)
-    daily_expenses = book.worksheets[0]  # позиционирование на листе
-    # all_category = {'Продукты':{},'Транспорт':{}}
-    all_category = {'транспорт': {},
-                    'еда': {},
-                    'здоровье, красота, гигиена': {},
-                    'инвестиции': {},
-                    'квартира': {},
-                    'продукты': {},
-                    'прочее': {},
-                    'рестораны': {},
-                    'связь': {},
-                    'алкоголь': {},
-                    'кино, театры, музеи': {},
-                    'одежда': {},
-                    'творчество, книги, обучение': {}}
-    border_of_month = find_border_of_month(month, year, daily_expenses)
-    for row in range(border_of_month[0], border_of_month[1] + 1):
-        category = daily_expenses[row][4].value.lower()
-        #_category = daily_expenses[row][5].value.lower()
-        _category = daily_expenses[row][5].value
-        if _category is None and all_category[category] == {}:
-            all_category[category].setdefault('noncategory', daily_expenses[row][6].value)
-        elif _category is None and all_category[category] != {}:
-            all_category[category]['noncategory'] = daily_expenses[row][6].value + all_category[category]['noncategory']
-        elif _category.lower() in all_category[category].keys():
-            all_category[category][_category.lower()] = all_category[category][_category.lower()] + daily_expenses[row][6].value
-        else:
-            all_category[category].setdefault(_category.lower(), daily_expenses[row][6].value)
-    book.close()
-    if write_exel:
-        write_category(month, year, all_category)
-    return all_category
-
-
-def find_border_of_month(month: int, year: int, exel_list):
-    """
-    :param month: месяц, для которого надо найти границы
-    :param year: год месяца, для которого надо найти границы
-    :return: возвращает list с двумя числами: начало месяца в строке, конец месяца в строке
-    """
-    start_list = left_bound(month, year, exel_list)
-    end_list =  right_bound(month, year, exel_list, start_list)
-    return [start_list,end_list]
-
-
-def left_bound(month: int, year: int, exel_list):
-    left = 2
-    right = exel_list.max_row
-    while exel_list[right][2].value.month - exel_list[left][2].value.month != 0:
-        if exel_list[left + 1][2].value.month == month and exel_list[left + 1][2].value.day == 1:
-            return left + 1
-        middle = (left + right )//2
-        if exel_list[middle][2].value.month < month:
-            left = middle
-        else:
-            right = middle
-    return left
-
-def right_bound(month: int, year: int, exel_list, start_list: int):
-    left = start_list #необходимо, чтобы всегда присутствовал первое число месяца
-    right = exel_list.max_row - 1
-    while exel_list[right][2].value.month - exel_list[left][2].value.month != 0:
-        if exel_list[left + 1][2].value.month == month + 1 and exel_list[left + 1][2].value.day == 1:
-            return left +1
-        middle = (left + right )//2
-        if exel_list[middle][2].value.month <= month:
-            left = middle
-        else:
-            right = middle
-    return right
-
-
-def write_category(month: int, year: int, all_category):
-    """
-    :param month: месяц подсчётов
-    :param year: год подсчётов
-    :param all_category: набор категорий
-    :return:
-    """
-    #book = openpyxl.open("test.xlsx", read_only=False)
-    book = openpyxl.open("C:/Users/apec9/Dropbox/ExelExpenses/test.xlsx", read_only=False)
-    list2 = book.worksheets[1]
-    begin_category_list_col = 10
-    for column in range(begin_category_list_col + 2, list2.max_column):
-        if list2[2][column].value.month == month and list2[2][column].value.year == year:
-            number_of_column = column - begin_category_list_col
-    #row = 2
-    row = 3
-    while row <= list2.max_row + 1 or len(all_category) != 0:
-        for category in all_category.keys():
-            leven_length = levenstein(list2[row][begin_category_list_col].value, category)
-            if leven_length == 0 or leven_length == 1:
-                list2[row][begin_category_list_col + number_of_column].value = sum(all_category[category].values())
-                position = 0
-                while len(all_category[category]) != 0:
-                    leven_length_ = levenstein(list2[row + 1][begin_category_list_col + 1].value,
-                                               list(all_category[category].keys())[position])
-                    if leven_length_ == 0 or leven_length_ ==1 and \
-                            position <= len(all_category[category]):
-                        list2[row + 1][begin_category_list_col + number_of_column].value = all_category[category][
-                            list(all_category[category].keys())[position]]
-                        all_category[category].pop(list(all_category[category].keys())[position])
-                        row += 1
-                        position = 0
-                    elif leven_length_ == None and \
-                            list(all_category[category].keys())[position] == 'noncategory':
-                        list2[row + 1][begin_category_list_col + number_of_column].value = all_category[category][
-                            list(all_category[category].keys())[position]]
-                        all_category[category].pop(list(all_category[category].keys())[position])
-                        row += 1
-                        position = 0
-                    elif position + 1 == len(all_category[category]):
-                        list2[row + 1][begin_category_list_col + number_of_column].value = 0
-                        position = 0
-                        row += 1
-                    else:
-                        position += 1
-                all_category.pop(category)
-                break
-        row += 1
-
-    book.save("test.xlsx")
-    book.close()
-    return print('Расходы записаны')
-
-
-def levenstein(A, B):
-    if A == None:
-        return None
-    F = [[(i + j) if i * j == 0 else 0 for j in range(len(B) + 1)]
-         for i in range(len(A) + 1)]
-    for i in range(1, len(A) + 1):
-        for j in range(1, len(B) + 1):
-            if A[i - 1] == B[j - 1]:
-                F[i][j] = F[i - 1][j - 1]
+                return [start_year_line, middle]
+            middle = (_position_of_start_line + _position_of_end_line) // 2
+            print(middle)
+            if self.list_number[middle][_position_of_general_data_column].value.month > month:
+                _position_of_end_line = middle
             else:
-                F[i][j] = 1 + min(F[i - 1][j], F[i][j - 1], F[i - 1][j - 1])
-    return F[len(A)][len(B)]
+                _position_of_start_line = middle
 
 
+    def category_counter(self, start_end_line):
+        pb = progressbar.progressbar(range(start_end_line[0], start_end_line[1] + 1))
+        _column_category = 4
+        _column_under_category = 5
+        _column_category_value = 6
+        for row in range(start_end_line[0], start_end_line[1] + 1):
+            next(pb)
+            category = self.list_number[row][_column_category].value.lower()
+            if category not in self._storage_of_category:
+                self._storage_of_category.setdefault(category, {})
+            if self.list_number[row][_column_under_category].value == None and len(self._storage_of_category[category]) == 0:
+                self._storage_of_category[category].setdefault('noncategory', 0)
+            if self.list_number[row][_column_under_category].value == None and len(self._storage_of_category[category]) != 0:
+                self._storage_of_category[category].setdefault('noncategory', self.list_number[row][_column_category_value].value)
+                self._storage_of_category[category]['noncategory'] =  \
+                    self._storage_of_category[category]['noncategory'] + self.list_number[row][_column_category_value].value
+            elif self.list_number[row][_column_under_category].value.lower() not in self._storage_of_category[category]:
+                self._storage_of_category[category].setdefault(self.list_number[row][_column_under_category].value.lower(),self.list_number[row][_column_category_value].value)
+            elif self.list_number[row][_column_under_category].value.lower()  in self._storage_of_category[category]:
+                self._storage_of_category[category][self.list_number[row][_column_under_category].value.lower()] =  \
+                    self._storage_of_category[category][self.list_number[row][_column_under_category].value.lower()]\
+                                                                          + self.list_number[row][_column_category_value].value
+        summa = []
+        all_category = list(self._storage_of_category.keys())
+        for i in range(0, len(self._storage_of_category)):
+            all_category_ = list(self._storage_of_category[all_category[i]].keys())
+            for j in all_category_:
+                summa.append(self._storage_of_category[all_category[i]][j])
+        print('Контрольная сумма:', sum(summa))
+        a=1
 
 
-#print(levenstein('Жкх', 'жкх'))
 xui = ExelExpenses()
-a = xui.expenses_processing(3, 2022)
+a = xui.expenses_processing(11, 2022)
 
 
 
-#category_counter(11, 2022, True)
